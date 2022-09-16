c9545c2bac8eb1feda6459d342a156a8c30dc275
Evgenii Tiurin <lawyertyurin@gmail.com>
2022-09-16 14:11:06 +0300

    Added Common Example

---
+++ CommonExample/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] nums = new int[10]{1, 2, 3, 4, 5, 6, 7, 8, 9, 30};
+int max = nums[0];
+for(int i = 0; i < nums.Length; i++) // индекс = 0, индекс < 10, индекс = индекс + 1
+{
+    if(nums[i] > max)
+    {
+        max = nums[i];
+    }
+}
+Console.WriteLine(max);
