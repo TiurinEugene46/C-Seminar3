67c5071e2d3f2a9699d6081926864e6d49f4f7cc
Evgenii Tiurin <lawyertyurin@gmail.com>
2022-09-16 14:45:24 +0300

    Добавлено решение задачи 17

---
+++ ATask17/Program.cs
@@ -0,0 +1,31 @@
+﻿// Определение четверти точки
+int GetPoinArea(int x, int y)
+{
+    if (x > 0 && y > 0)
+    {
+        return 1;
+    }
+    if (x < 0 && y > 0)
+    {
+        return 2;
+    }
+    if (x < 0 && y < 0)
+    {
+        return 3;
+    }
+    if (x > 0 && y < 0)
+    {
+        return 4;
+    }
+
+    return 0;
+}
+
+Console.Write("Введите число X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int areaNum = GetPoinArea(x, y);
+Console.WriteLine($"Area = {areaNum}");
