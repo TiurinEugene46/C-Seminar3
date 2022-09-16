09b7768b28b4b647f5cd14319e0185c77507401e
Evgenii Tiurin <lawyertyurin@gmail.com>
2022-09-16 15:04:17 +0300

    Добавлено решение задачи 18

---
+++ ATask18/Program.cs
@@ -0,0 +1,27 @@
+﻿// Определение четверти точки
+string GetAreaRange(int areaNum)
+{
+    if (areaNum == 1){
+        return  "x > 0 y > 0";
+    }
+    if (areaNum == 2){
+        return "x < 0 y > 0";
+    }
+    if (areaNum == 3){
+     return "x < 0 y < 0";
+    }
+    if (areaNum == 4){
+        return "x > 0 y < 0";
+    }
+    if(areaNum == 0){
+        return "x = 0 y = 0";
+    }
+
+    return "Введите номер четверти больше 4-х";
+}
+
+Console.Write("Введите число: ");
+int areaNum = Convert.ToInt32(Console.ReadLine());
+
+string result = GetAreaRange(areaNum);
+Console.Write(result);
